61e06d9c2823fc38f22b3b1d1104e29e63e41b2b
CaptainHmD <hamad.laaboun@gmail.com>
2023-06-19 05:43:23 +0300

    change port to match vps

--- test/app.js
+++ test/app.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
   res.sendFile("chat-room.html", { root: "./public/view" });
 });
 
-server.listen(3500, () => {
+server.listen(3200, () => {
   console.log("Server is listing");
 });
 
